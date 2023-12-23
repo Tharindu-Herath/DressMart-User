add79dfd74afd332f193e6cc773407d7aab922cb
Tharindu Herath <slboxerme@gmail.com>
2023-12-23 03:11:26 +0300

    first commit

---
+++ components/ui/billboard.tsx
@@ -0,0 +1,30 @@
+import type { Billboard } from "@/types";
+
+interface BillboardProps {
+  data: Billboard | null; // Make sure data can be null
+}
+
+const Billboard: React.FC<BillboardProps> = ({ data }) => {
+  // Check if data is null before accessing its properties
+  if (!data) {
+    // You can return some default content or a placeholder here
+    return <div>No data available</div>;
+  }
+
+  return (
+    <div className="p-4 sm:p-6 lg:p-8 rounded-xl overflow-hidden">
+      <div
+        style={{ backgroundImage: `url(${data?.imageUrl})` }}
+        className="rounded-xl relative aspect-square md:aspect-[2.4/1] overflow-hidden bg-cover"
+      >
+        <div className="h-full w-full flex flex-col justify-center items-center text-center gap-y-8">
+          <div className="font-bold text-3xl sm:text-5xl lg:text-6xl sm:max-w-xl max-w-xs">
+            {data.label}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Billboard;
